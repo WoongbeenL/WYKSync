81f61cba536af34650f09340b28fea6c2549ef1f
Ktheoret <Kristin.Theoret@gmail.com>
2026-01-31 14:44:22 -0500

    Added a makeshift logo while we are finalizing our own logo.

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,10 @@
 import { Children } from "react";
+import fishImg from "../assets/fish.jpg";
 
 export default function Navbar() {
   return (
     <nav className="nav">
+      <img src={fishImg} alt="Logo" className="logo" />
       <a href="/" className="site-title">
         WYKSync
       </a>
@@ -21,7 +23,9 @@ function CustomLink({ href, children, ...props }) {
   const path = window.location.pathname;
   return (
     <li className={path === href ? "active" : ""}>
-      <a href={href}{...props}>{children}</a>
+      <a href={href} {...props}>
+        {children}
+      </a>
     </li>
   );
 }
