fc2540f20c435f62535ca557d32c9082261b3011
Ktheoret <Kristin.Theoret@gmail.com>
2026-02-01 18:02:04 -0500

    Started building the home page with explaining how we started whats our goal etc. wording needs to be polished.

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,3 +1,22 @@
-export default function Home(){
-    return <h1>HOME</h1>
+export default function Home() {
+  return (
+    <>
+    <h1>HOME</h1>
+    <p>
+      Our goal is to take overlays for valorant to another level of improvement.
+    </p>
+    <p>
+      We want to have the ability to live update information and make things
+      easier for broadcasts.
+    </p>
+    <p>
+      We are a group of 3 developers who started by working with valorant
+      broadcasts first hand at our esports facility.
+    </p>
+    <p>
+        We strive for an easier overlay solution for broadcasts all around.
+    </p>
+    
+  </>
+  );
 }
