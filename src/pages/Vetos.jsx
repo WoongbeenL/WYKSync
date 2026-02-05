2deecf32864fda5ac5e1eb717d489536491ab238
Ktheoret <Kristin.Theoret@gmail.com>
2026-02-05 11:41:56 -0500

    started working on vetos page for website starting with the base setups and wireframes along with adding buttons

--- src/pages/Vetos.jsx
+++ src/pages/Vetos.jsx
@@ -1,3 +1,12 @@
 export default function Vetos(){
-    return <h1>Vetos</h1>
+    return (
+        <>
+        <h1>Vetos</h1>
+        <p>Explaining here how vetos in tournaments will be linked to their specific bracket/game.</p>
+       <div className="decider-buttons">
+            <button>Coin Flip</button>
+            <button>Manual Team Selection</button>
+        </div>   
+        </>
+    );
 }
