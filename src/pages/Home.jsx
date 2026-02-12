e62917eea40be57fe61af5669c5d0476cc6a021e
Ktheoret <Kristin.Theoret@gmail.com>
2026-02-12 12:05:09 -0500

    modified the wording for the landing page as well as started building tournaments page. Very Basic.

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -7,7 +7,7 @@ export default function Home() {
         <h1>WYKSync</h1>
         <h3>Real-Time Valorant Broadcast Overlays</h3>
         <p>
-          Short paragraph about our project. Short paragraph about our project. Short paragraph about our project. Short paragraph about our project.
+          WYKSync is an esports broadcast management system developed for the PROG3321 Systems Project course.
         </p>
         <div className="hero-buttons">
             <button>Launch Overlay</button>
@@ -17,23 +17,33 @@ export default function Home() {
     <section className="section">
       <h2>The Problem</h2>
       <p>
-        talk about manual input being an issue and using multiple tools to keep overlays updated needing the have brackets from tournament websites along with map vetos etc.
+        WYKSync was created to simplify and help improve esports and collegiate broadcasts.
+
+        Live productions often require multiple tools to manage tournaments, match data, overlays, and statistics making a lot of manual input required for the broadcast teams.
+        This increases the risk of human error, and adds stress to production teams during live events.
+
+        The purpose of WYKSync is to bring all the tools needed to run an event in one place allowing production staff to focus on gameplay and viewer experience rather than manual data entry.
       </p>
       
       <h2>Our Solution</h2>
       <p>
-        what we plan to fix and resolve. talk about the connection between this application and our overlay. talk about the vetos being more readable.
+        WYKSync provides an all-in-one broadcast management system that integrates tournament organization, live match data, and overlay control.
+        We are also upgrading and modifying existing overlay controls to improve the observers experience such as the ability to keep HUD controls from the game like player correspondance to number keeping it only on the observers side as well as keeping observers information such as timeouts and tech pauses.
+    
       </p>
     </section>
     
     <section className="features">
       <h2>Platform Features</h2>
-      <p>insert features from our document report</p>
       <ul>
-        <li>one</li>
-        <li>two</li>
-        <li>three</li>
-        <li>four</li>
+        <li>Manage Tournaments</li>
+        <li>Manage Teams</li>
+        <li>Manage Matches</li>
+        <li>Manage Statistics</li>
+        <li>Manage Leaderboards</li>
+        <li>Manage Vetoes</li>
+        <li>Manage Overlay</li>
+        <li>Pull live Match data</li>
       </ul>
     </section>
 
@@ -41,6 +51,15 @@ export default function Home() {
       <h2>Meet the Team</h2>
       <p>
         Built by a student team of three developers with hands on experiences within the esports production world.
+        <h2>Frontend Developer</h2>
+        <p>Kristin Theoret</p>
+        <p>LinkedIn Link</p>
+        <h2>Backend Developer</h2>
+        <p>Will Lee</p>
+        <p>LinkedIn Link</p>
+        <h2>Team Lead Developer</h2>
+        <p>Ygnacio Maza Sanchez</p>
+        <p>LinkedIn Link</p>
       </p>
 
       <h2>Ready to start broadcasting smarter?</h2>
--- src/pages/Tournaments.jsx
+++ src/pages/Tournaments.jsx
@@ -1,3 +1,12 @@
 export default function Tournaments(){
-    return <h1>Tournaments</h1>
+    return (
+        <div>
+             <h1>Tournaments</h1>
+             <button>Add Tournament</button>
+            <ul>
+                <li>tournaments1?</li>
+                <li>tournaments2?</li>
+            </ul>
+        </div>
+    );
 }
