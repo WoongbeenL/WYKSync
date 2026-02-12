a51b47e96c028b434225c4fc87f2125b57038540
WoongbeenL <ultrawoong2@gmail.com>
2026-02-12 18:32:19 -0500

    Create Backend_Server and server.js
    Created the backend server with minimal code in server.js with Node.js and Express.

---
+++ Backend_Server/server.js
@@ -0,0 +1,23 @@
+/*
+* Filename     : server.js
+* Project      : PROG3221 - Capstone Project
+* Programmers  : Will Lee
+* Date         : 2/12/2026
+* Description  : This is a Node.js file to host a backend server for WYKSync.
+*/
+
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.get("/health", (req, res) => {
+  res.send("Backend running");
+});
+
+const PORT = process.env.PORT || 10000;
+app.listen(PORT, "0.0.0.0", () =>
+  console.log(`Server running on ${PORT}`)
+);
