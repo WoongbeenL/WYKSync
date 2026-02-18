91767284b581689af79525c437a8d977aa3f2cf1
WoongbeenL <ultrawoong2@gmail.com>
2026-02-18 15:00:08 -0500

    Modify supabase.js
    Added error checking for Supabase env variables

--- Backend_Server/lib/supabase.js
+++ Backend_Server/lib/supabase.js
@@ -6,13 +6,18 @@
 * Description  : This is a js file that creates a supabase module to be used by middleware and routes.
 */
 
-const { createClient } = require("@supabase/supabase-js")
+const { createClient } = require("@supabase/supabase-js");
+
+const supabaseUrl = process.env.SUPABASE_URL;
+const supabaseKey = process.env.SUPABASE_ANON_KEY;
+
+// Ensure Supabase URL and Supabase Anon Key exists as env variables
+if (!supabaseUrl || !supabaseKey) {
+    throw new Error('Missing Supabase Environment Variables');
+}
 
 // Using createClient function from Supabase
-const supabase = createClient(
-    process.env.SUPABASE_URL,
-    process.env.SUPABASE_ANON_KEY
-);
+const supabase = createClient(supabaseUrl, supabaseKey);
 
 // Export to be used in other files
 module.exports = supabase;
